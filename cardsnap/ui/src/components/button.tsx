d31f92f550c7436aab09819b10a2ed991b53b45f
Magnus Tomter Ouren <magnutou@stud.ntnu.no>
2022-11-10 10:56:28 +0100

    Add POST to API when creating deck

--- cardsnap/ui/src/components/button.tsx
+++ cardsnap/ui/src/components/button.tsx
@@ -9,19 +9,16 @@ interface ButtonProps {
 export const Button: FC<ButtonProps> = ({ clickHandler = () => null, label, type = 'default' }) => {
   const getColorClass = () => {
     if (type === 'red') {
-      return 'bg-red-400 active:bg-red-500 border-2 border-red-400 active:border-red-500';
+      return 'bg-red-400 text-white active:bg-red-500 border-2 border-red-400 active:border-red-500';
     }
     if (type === 'gray') {
       return 'bg-none text-gray-500 border-gray-400 border-2';
     }
-    return 'bg-sky-500 active:bg-sky-600 border-2 border-sky-500 active:border-sky-600';
+    return 'bg-sky-500 text-white active:bg-sky-600 border-2 border-sky-500 active:border-sky-600';
   };
 
   return (
-    <button
-      onClick={clickHandler}
-      className={`text-white py-2 w-32 transition duration-100 ease-in-out ${getColorClass()}`}
-    >
+    <button onClick={clickHandler} className={` py-2 w-32 transition duration-100 ease-in-out ${getColorClass()}`}>
       {label}
     </button>
   );
--- cardsnap/ui/src/views/createDeckPage/createDeckForm.tsx
+++ cardsnap/ui/src/views/createDeckPage/createDeckForm.tsx
@@ -1,23 +1,59 @@
-import { FC, FormEventHandler } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosResponse } from 'axios';
+import { FC, useState } from 'react';
+import { FeecbackContainer } from './feedbackContainer';
+import { Button } from '../../components/button';
+import { DECKS_ENDPOINTS } from '../../helpers/api';
+import { CardDeck } from '../../helpers/mockData';
 
 export const CreateDeckForm: FC = () => {
-  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
+  const [displayFeedBack, setDisplayFeedback] = useState(false);
+  const [feedbackText, setFeedbackText] = useState('');
+  const [newDeckName, setNewDeckName] = useState('');
+
+  const createDeckCall = async () => {
+    const res: AxiosResponse<CardDeck> = await axios.post(DECKS_ENDPOINTS, { name: newDeckName });
+    return res.data;
   };
+
+  const { mutate, isSuccess, data } = useMutation(createDeckCall, {
+    onSuccess: (data) => {
+      setFeedbackText(`${data.name} was added!`);
+      setDisplayFeedback(true);
+      setNewDeckName('');
+    },
+    onError: () => {
+      setFeedbackText('Error when creating deck');
+      setDisplayFeedback(true);
+    },
+  });
+
   return (
-    <form onSubmit={onSubmit} className="bg-white border-4 p-4 w-80 shadow-md">
-      <label htmlFor="deckName" className="block mb-2 text-sm font-medium text-gray-900">
-        Name
+    <>
+      <div className="bg-white border-4 p-4 w-80 shadow-md">
+        <label htmlFor="deckName" className="block mb-4 font-medium text-gray-900">
+          Name
+        </label>
         <input
           type="text"
           id="deckName"
           name="deckName"
-          placeholder="deckname"
-          className="mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
+          placeholder="New deckname"
+          className="w-full mb-4 bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 p-2"
+          value={newDeckName}
+          onChange={(evt) => setNewDeckName(evt.target.value)}
           required
         />
-      </label>
-      <input type="submit" value="Create" className="cursor-pointer text-white bg-sky-500 w-32 py-2 mt-4" />
-    </form>
+        <Button label="Create" clickHandler={mutate} />
+      </div>
+      {displayFeedBack && (
+        <FeecbackContainer
+          setDisplayFeedback={setDisplayFeedback}
+          feedbackText={feedbackText}
+          isSucess={isSuccess}
+          data={data}
+        />
+      )}
+    </>
   );
 };

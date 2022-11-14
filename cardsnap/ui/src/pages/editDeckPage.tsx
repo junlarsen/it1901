357aa6453532b44d4322eee8873dac9e8c75012e
Tale Nygaard Eikenes <talene@stud.ntnu.no>
2022-11-14 18:25:30 +0100

    Implement delete deck component

--- cardsnap/ui/src/pages/editDeckPage.tsx
+++ cardsnap/ui/src/pages/editDeckPage.tsx
@@ -4,13 +4,17 @@ import { CreateCardContainer } from '../views/editDeckPage/createCardContainer';
 import { EditDeckPageHeader } from '../views/editDeckPage/editDeckPageHeader/editDeckPageHeader';
 import { useQuery } from '@tanstack/react-query';
 import axios, { AxiosResponse } from 'axios';
+import { CreateCardContainer } from '../views/editDeckPage/createCardContainer';
+import { EditDeckPageHeader } from '../views/editDeckPage/editDeckPageHeader/editDeckPageHeader';
 import { DECKS_ENDPOINTS } from '../helpers/api';
 import { CardDeck } from '../helpers/mockData';
 import { DeckNotFound } from '../components/deckNotFound';
 import { ListOfCards } from '../views/editDeckPage/listOfCards';
+import { DeleteDeckButton } from '../views/editDeckPage/deleteDeckButton';
 
 export const EditDeckPage: FC = () => {
-  const params = useParams<Params<'id'>>();
+  const params = useParams<Params<'id'>>;
+  console.log(params.toString());
 
   const { isLoading, data, isSuccess, isError, refetch } = useQuery(['deck', params.id], () =>
     axios.get(DECKS_ENDPOINTS + params.id).then((res: AxiosResponse<CardDeck>) => res.data),
@@ -25,6 +29,7 @@ export const EditDeckPage: FC = () => {
             <EditDeckPageHeader deck={data} refetch={refetch} />
             <CreateCardContainer deckName={data.name} />
             <ListOfCards id={data.id} />
+            <DeleteDeckButton />
           </>
         )}
         {isError && <DeckNotFound id={params.id} />}

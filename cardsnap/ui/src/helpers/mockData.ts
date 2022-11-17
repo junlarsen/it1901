174a4ba7d6ee055f875360b60efe4abf4c35289e
Mats Jun Larsen <matsjla@stud.ntnu.no>
2022-11-17 16:07:51 +0100

    Add back mock data for use in tests

--- cardsnap/ui/src/helpers/mockData.ts
+++ cardsnap/ui/src/helpers/mockData.ts
@@ -13,3 +13,24 @@ export interface CardDeck {
   name: string;
   id: string;
 }
+
+export const cardDecks: CardDeck[] = [
+  {
+    name: 'Algdat',
+    id: 'algdat',
+  },
+  {
+    name: 'ITP',
+    id: 'itp',
+  },
+  {
+    name: 'Endringsagent',
+    id: 'endringsagent',
+  },
+  {
+    name: 'DatDig',
+    id: 'datadig',
+  },
+];
+
+export const mockCard: Card = { question: 'unit', answer: 'test', id: '1', owner: 'itp' };
